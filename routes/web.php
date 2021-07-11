f6869c68601cfa58ff1bf80e6b338f65208a282a
akasifislam <aseasifislam@gmail.com>
2021-07-11 10:50:42 +0600

    file upload with out image

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+
+Route::resource('/albums', AlbumController::class);
